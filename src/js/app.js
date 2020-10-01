96c3b213fd9412b0be4f460fc6bc29e8b9cf0b71
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-10-01 11:25:13 +0300

    Запутался

--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,8 @@ let edit = document.getElementsByClassName('edit');
 let editNameInput = document.getElementById('edit_name_input');
 let editPriceInput = document.getElementById('edit_price_input');
 let counter = 0;
+let items = document.getElementsByClassName('item');
+let deleter = document.getElementsByClassName('delete');
 
 function addItem() {
     let table = document.getElementById('table');
@@ -15,6 +17,7 @@ function addItem() {
         let nameField = addForm.name,
         priceField = addForm.price;
         let obj = {
+            id: counter,
             name: nameField.value,
             price: +priceField.value
         };
@@ -22,12 +25,12 @@ function addItem() {
 
     
         
-        table.insertAdjacentHTML('beforeend', `<tr id=${counter}>
+        table.insertAdjacentHTML('beforeend', `<tr>
         <td class="item_name">${obj.name.trim()}</td>
         <td class="item_price">${obj.price}</td>
         <td class="item_actions">
-            <span class="edit">&#9998;</span>
-            <span class="delete">&#65794;</span>
+            <span class="edit" data-index="${obj.id}">&#9998;</span>
+            <span class="delete" data-number="${obj.id}">&#65794;</span>
         </td>
     </tr>`);
     arr.push(obj)
@@ -35,16 +38,18 @@ function addItem() {
         counter++;
 
         if (arr.length > 0) {
-
-                for(let j=0; j<arr.length; j++) {
-                edit[i].addEventListener('click', (e) => {
+            for (let ed of edit) {
+                for (let j of arr) {
+                  
+                    
+                ed.addEventListener('click', function(e) {
                     editItemModal.classList.add('active');
-                    console.log(e.currentTarget.closest('tr'))
-                    // if (j === counter) {
-                    editNameInput.value = 'Заполни меня';
-                    editPriceInput.value = 'И моего сына тоже';
-                    // }
-                });
+                    console.log(e.currentTarget.dataset.index)
+                    if (e.currentTarget.dataset.index == this.id) {
+                    editNameInput.value = this.name;
+                    editPriceInput.value = this.price;
+                }});
+            
                 editForm.addEventListener('submit', () => {
                     // editForm.reset();
                     
@@ -54,10 +59,24 @@ function addItem() {
                 editForm.addEventListener('reset', () => {
                     editItemModal.classList.remove('active')
                 })
+                for (let x of deleter) {
+                    x.addEventListener('click', function(e) {
+                        if (e.currentTarget.dataset.number == j.id) {
+
+                        e.currentTarget.parentElement.parentElement.remove();
+                        arr.splice(j, 1);
+                        console.log(j, arr.length)
+
+                        }
+                    })
+                }
             }
-        
+            
         }
+
+    }
 }
+
 
 add.addEventListener('click', () => {
     newItemModal.classList.add('active')
