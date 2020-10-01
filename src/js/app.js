296c9cfda26c0e71b9744969e1b22bc40fc26963
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-10-01 15:02:34 +0300

    Исправил все ошибки линтера

--- src/js/app.js
+++ src/js/app.js
@@ -1,62 +1,62 @@
-let arr = [];
-let add = document.querySelector(".add");
-let addForm = document.getElementById("add_form");
-let editForm = document.getElementById("edit_form");
-let newItemModal = document.querySelector(".modal_add");
-let editItemModal = document.querySelector(".modal_edit");
-let edit = document.getElementsByClassName("edit");
-let editNameInput = document.getElementById("edit_name_input");
-let editPriceInput = document.getElementById("edit_price_input");
+const arr = [];
+const add = document.querySelector('.add');
+const addForm = document.getElementById('add_form');
+const editForm = document.getElementById('edit_form');
+const newItemModal = document.querySelector('.modal_add');
+const editItemModal = document.querySelector('.modal_edit');
+const edit = document.getElementsByClassName('edit');
+const editNameInput = document.getElementById('edit_name_input');
+const editPriceInput = document.getElementById('edit_price_input');
 let counter = 0;
-let items = document.getElementsByClassName("item");
-let deleter = document.getElementsByClassName("delete");
+// const items = document.getElementsByClassName('item');
+// const deleter = document.getElementsByClassName('delete');
 
 function addItem() {
-  let table = document.getElementById("table");
+  const table = document.getElementById('table');
 
-  let nameField = addForm.name,
-    priceField = addForm.price;
-  let obj = {
+  const nameField = addForm.name;
+  const priceField = addForm.price;
+  const obj = {
     id: counter,
     name: nameField.value,
     price: +priceField.value,
   };
 
   table.insertAdjacentHTML(
-    "beforeend",
+    'beforeend',
     `<tr>
-        <td class="item_name">${obj.name.trim()}</td>
-        <td class="item_price">${obj.price}</td>
-        <td class="item_actions">
-            <span class="edit" data-index="${counter}">&#9998;</span>
-            <span class="delete" data-index="${counter}">&#65794;</span>
+        <td class='item_name'>${obj.name.trim()}</td>
+        <td class='item_price'>${obj.price}</td>
+        <td class='item_actions'>
+            <span class='edit' data-index='${counter}'>&#9998;</span>
+            <span class='delete' data-index='${counter}'>&#65794;</span>
         </td>
-    </tr>`
+    </tr>`,
   );
   arr.push(obj);
   addForm.reset();
-  counter++;
+  counter += 1;
 
   if (arr.length > 0) {
-    for (let ed of edit) {
-      for (let j of arr) {
-        ed.addEventListener("click", function (e) {
-          editItemModal.classList.add("active");
-          if (e.currentTarget.dataset.index == j.id) {
+    for (const ed of edit) {
+      for (const j of arr) {
+        ed.addEventListener('click', (e) => {
+          editItemModal.classList.add('active');
+          if (e.currentTarget.dataset.index === j.id) {
             editNameInput.value = j.name;
             editPriceInput.value = j.price;
           }
           console.log(j.id);
         });
 
-        editForm.addEventListener("submit", () => {
+        editForm.addEventListener('submit', () => {
           // editForm.reset();
 
           j.name = editNameInput.value;
           j.price = editPriceInput.value;
         });
-        editForm.addEventListener("reset", () => {
-          editItemModal.classList.remove("active");
+        editForm.addEventListener('reset', () => {
+          editItemModal.classList.remove('active');
         });
         // for (let x of deleter) {
         //     x.addEventListener('click', function(e) {
@@ -72,15 +72,15 @@ function addItem() {
   }
 }
 
-add.addEventListener("click", () => {
-  newItemModal.classList.add("active");
+add.addEventListener('click', () => {
+  newItemModal.classList.add('active');
 });
 
-addForm.addEventListener("submit", (e) => {
+addForm.addEventListener('submit', (e) => {
   e.preventDefault();
   addItem();
-  newItemModal.classList.remove("active");
+  newItemModal.classList.remove('active');
 });
-addForm.addEventListener("reset", () => {
-  newItemModal.classList.remove("active");
+addForm.addEventListener('reset', () => {
+  newItemModal.classList.remove('active');
 });
